2de826fa577e4284bae9848c7e3e6d88fbae5811
MicHaeLNgSd <koratgorovich2017@gmail.com>
2023-01-12 18:17:30 +0200

    homeworkDAY1

--- src/index.js
+++ src/index.js
@@ -37,15 +37,59 @@ createApp({
             piece: '',
             search: '',
             student: { "name": "", isDonePr: false, group: "" },
+
+
+            convertorData: [],
+            convAmount: 1,
+            convAnswer: 1,
+
+            convHH: 1,
+            convDD: 1,
+            convEE: 1,
+
+            convHD: 1,
+            convDH: 1,
+
+            convHE: 1,
+            convEH: 1,
+
+            convED: 1,
+            convDE: 1,
+
+            convFrom: 'D',
+            convTo: 'H',
         }
     },
     mounted: function () {
-
         axios.get("http://34.82.81.113:3000/students").then((res) => {
             console.log(res.data);
             this.students = res.data;
-        })
+        });
+
+        axios.get("https://api.monobank.ua/bank/currency").then((res) => {
+            console.log(res.data);
+            this.convertorData = res.data;
+
+            this.convDH = this.convertorData[0].rateBuy;
+            this.convHD = 1 / this.convertorData[0].rateSell;
+
+            this.convEH = this.convertorData[1].rateBuy;
+            this.convHE = 1 / this.convertorData[1].rateSell;
+
+            this.convED = this.convertorData[2].rateBuy;
+            this.convDE = 1 / this.convertorData[2].rateSell;
+        });
+
+        // axios.get("https://apilayer.com/marketplace/exchangerates_data-api#documentation-tab",
+        //     {
+        //         headers: {
+        //             "apikey": "0sjTljESXiA3z56QG2VZ0P0Rjp7wGkfg"
+        //         },
+        //     }).then((res) => {
+        //         console.log(res.data);
+        //     })
     },
+
     methods: {
         deleteStudent(studId) {
             this.students = this.students.filter(elem => {
@@ -55,7 +99,10 @@ createApp({
         addStudent() {
             this.student._id = this.students.length + 1;
             this.students.push({ ...this.student });
+        },
+        convert() {
+            console.log(`conv${this.convFrom}${this.convTo}`);
+            this.convAnswer = this.convAmount * this[`conv${this.convFrom}${this.convTo}`]
         }
     },
-
 }).mount('#app')
